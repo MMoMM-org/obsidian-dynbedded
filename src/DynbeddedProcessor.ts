1195bac8c8b7f465b15c9181cb6862c3fd1e6ec5
MMoMM <marcus@mmomm.org>
2022-10-21 13:27:44 +0200

    refactor: refactored DynbeddedProcessor.ts
    created some methods to remove complexity.. still a lot to do

--- src/DynbeddedProcessor.ts
+++ src/DynbeddedProcessor.ts
@@ -1,4 +1,4 @@
-import {App, MarkdownPostProcessorContext, MarkdownRenderer} from "obsidian";
+import {App, MarkdownPostProcessorContext, MarkdownRenderer, TFile} from "obsidian";
 import Dynbedded from "./main";
 
 export class DynbeddedProcessor {
@@ -16,36 +16,18 @@ export class DynbeddedProcessor {
         const fileNameMatch = fileNameMatchPattern.exec(source);
 
         this.plugin.log("FileNameMatch", fileNameMatch);
-
         if (!fileNameMatch) {
             Dynbedded.displayError(el, "Bad file link: " + source);
             return;
         }
-        let fileName = fileNameMatch[1];
 
+        let fileName = fileNameMatch[1];
         const dynamicDateMatchPattern = /{{(.*)}}/;
         const dynamicDateMatch = dynamicDateMatchPattern.exec(fileName);
         this.plugin.log("DynamicDateMatch", dynamicDateMatch);
+
         if (dynamicDateMatch !== null) {
-            let dynamicDateFormat = dynamicDateMatch[1];
-            let duration = window.moment.duration(0);
-            this.plugin.log("DynamicDateFormat", dynamicDateFormat.includes("|"))
-            if (dynamicDateFormat.includes("|")) {
-                const offset = dynamicDateFormat.split("|")[1];
-                this.plugin.log("Offset", offset);
-                dynamicDateFormat = dynamicDateFormat.split("|")[0];
-                this.plugin.log("dynamicDateMatch", dynamicDateFormat)
-                if (/^-?\d+$/.test(offset)) {
-                    this.plugin.log("Number");
-                    duration = window.moment.duration(Number(offset), "days");
-                } else {
-                    this.plugin.log("String");
-                    duration = window.moment.duration(offset);
-                }
-                this.plugin.log("Duration", duration);
-            }
-            const dynamicDate = window.moment().add(duration).format(dynamicDateFormat);
-            this.plugin.log("DynamicDate", dynamicDate);
+            const {dynamicDateFormat, dynamicDate} = this.getDynamicDate(dynamicDateMatch);
             // Todo: figure out how to handle wrong formats correctly.. most formats are valid but create undesired results...
             if (!window.moment(window.moment.now(), dynamicDateFormat, true).isValid || dynamicDate === null) {
                 Dynbedded.displayError(el, "Not a valid Moment.js Time format: " + dynamicDateFormat);
@@ -54,15 +36,16 @@ export class DynbeddedProcessor {
             fileName = fileName.replace(dynamicDateMatchPattern, dynamicDate);
             this.plugin.log("DynamicFileName", fileName);
         }
+
         let header = "";
         if (fileName.contains("#")) {
-            header = fileName.split("#")[1];
-            fileName = fileName.split("#")[0];
-            this.plugin.log("Header", header);
+            const __ret = this.splitFileName(fileName);
+            header = __ret.header;
+            fileName = __ret.fileName;
         }
-
         const matchingFile = this.app.metadataCache.getFirstLinkpathDest(fileName, '');
         this.plugin.log("MatchingFile", matchingFile);
+
         if (!matchingFile) {
             Dynbedded.displayError(el, "File link not found: [[" + fileName + "]]");
             return;
@@ -80,6 +63,7 @@ export class DynbeddedProcessor {
             if (headings === null || headings === undefined) {
                 const errorMessage = "Header \"" + header + "\" not found in [[" + fileName + "]]";
                 Dynbedded.displayError(el, errorMessage);
+                return;
             }
             this.plugin.log("Headings", headings);
             let position;
@@ -95,14 +79,7 @@ export class DynbeddedProcessor {
                 }
             }
             if (position) {
-                let text = await this.app.vault.cachedRead(matchingFile)
-                if (!text.endsWith("\n")) {
-                    text = text + "\n"
-                }
-                this.plugin.log("Position", position);
-                this.plugin.log("Text", text);
-                fileContents = text.split("\n").slice(position[0] + 1, position[1]).join("\n");
-                this.plugin.log("Split", fileContents)
+                fileContents = await this.getHeaderSectionContent(matchingFile, position, fileContents);
             }
         } else {
             fileContents = await this.app.vault.cachedRead(matchingFile);
@@ -114,7 +91,49 @@ export class DynbeddedProcessor {
         }
         this.plugin.log("File", fileContents)
         const container = el.createDiv({cls: [Dynbedded.containerClass]});
-        await MarkdownRenderer.renderMarkdown(fileContents, container, ctx.sourcePath, this);
+        await MarkdownRenderer.renderMarkdown(fileContents, container, ctx.sourcePath, this.plugin);
+    }
+
+    private getDynamicDate(dynamicDateMatch: RegExpExecArray) {
+        let dynamicDateFormat = dynamicDateMatch[1];
+        let duration = window.moment.duration(0);
+        this.plugin.log("DynamicDateFormat", dynamicDateFormat.includes("|"))
+        if (dynamicDateFormat.includes("|")) {
+            const offset = dynamicDateFormat.split("|")[1];
+            this.plugin.log("Offset", offset);
+            dynamicDateFormat = dynamicDateFormat.split("|")[0];
+            this.plugin.log("dynamicDateMatch", dynamicDateFormat)
+            if (/^-?\d+$/.test(offset)) {
+                this.plugin.log("Number");
+                duration = window.moment.duration(Number(offset), "days");
+            } else {
+                this.plugin.log("String");
+                duration = window.moment.duration(offset);
+            }
+            this.plugin.log("Duration", duration);
+        }
+        const dynamicDate = window.moment().add(duration).format(dynamicDateFormat);
+        this.plugin.log("DynamicDate", dynamicDate);
+        return {dynamicDateFormat, dynamicDate};
+    }
+
+    private async getHeaderSectionContent(matchingFile: TFile, position: number[], fileContents: string) {
+        let text = await this.app.vault.cachedRead(matchingFile)
+        if (!text.endsWith("\n")) {
+            text = text + "\n"
+        }
+        this.plugin.log("Position", position);
+        this.plugin.log("Text", text);
+        fileContents = text.split("\n").slice(position[0] + 1, position[1]).join("\n");
+        this.plugin.log("Split", fileContents)
+        return fileContents;
+    }
+
+   private splitFileName(fileName: string) {
+        const header = fileName.split("#")[1];
+        fileName = fileName.split("#")[0];
+        this.plugin.log("Header", header);
+        return {header, fileName};
     }
 }
 
